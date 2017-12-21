7b276c3c8b9a8cd06e596a85287e79cf1847ac93
xijier <aola.xijier@gmail.com>
2017-12-21 22:44:52 +0800

    update server

--- app/controllers/DeviceMobileController.java
+++ app/controllers/DeviceMobileController.java
@@ -1,69 +1,129 @@
 package controllers;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Optional;
+import java.util.Random;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
+import org.h2.engine.Session;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 
 import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Controller;
 import play.mvc.Result;
 import repository.UserRepository;
+import scala.collection.immutable.StreamViewLike.EmptyView;
+import controllers.Base64_;
+import models.User;
 
-public class DeviceMobileController extends Controller{
+public class DeviceMobileController extends Controller {
 	private final UserRepository userRepository;
 	private final HttpExecutionContext httpExecutionContext;
-	 @Inject
-	    public DeviceMobileController(UserRepository userRepository,HttpExecutionContext httpExecutionContext) {
+	private Random random;
+	private static LoadingCache<String, String> cahceBuilder = CacheBuilder.newBuilder()
+			.expireAfterWrite(60, TimeUnit.SECONDS).maximumSize(100).concurrencyLevel(10)
+			.build(new CacheLoader<String, String>() {
+				@Override
+				public String load(String key) throws Exception {
+					return key;
+				}
+			});
 
-	        this.userRepository = userRepository;
-	        this.httpExecutionContext = httpExecutionContext;
-	    }
-	 
-	    public CompletionStage<Result> register(String username,String password)
-	    {
-	        System.out.println(username);
-	    	System.out.println(password);
-	        return userRepository.insert(username,password).thenApplyAsync(data -> {
-	            // This is the HTTP rendering thread context
-	            return ok("ok");
-	        }, httpExecutionContext.current());
-	    }
-	    
-	    public CompletionStage<Result> registeruserTest()
-	    {
-	    	JsonNode json = request().body().asJson();
-	    	String name = json.findPath("name").textValue();
-	    	System.out.println(name);
-	    	String passwordEn = json.findPath("password").textValue();
-	    	System.out.println(name);
-	    	System.out.println(passwordEn);
-	    	//String str2 = Base64_.base64decode(passwordEn);
-	    	//System.out.println(str2);
-	        return userRepository.insert(name,passwordEn).thenApplyAsync(data -> {
-	        	return ok("ok");
-	        }, httpExecutionContext.current());
-	    }
-	    
-	    public CompletionStage<Result> mobilelogin()
-	    {
-	    	JsonNode json = request().body().asJson();
-	    	String name = json.findPath("name").textValue();
-	    	String passwordEn = json.findPath("password").textValue();
-	    	//System.out.println(name);
-	    	//System.out.println(passwordEn);
-	    	//String decode = Base64_.base64decode(passwordEn);
-	    	//System.out.println("de code: "+str2);
-	    	return userRepository.validUserPassword(name,passwordEn).thenApplyAsync(data -> {
-	    		if(data.get())
-	    		{
-	    			return ok("ok");
-	    		}
-	    		else
-	    		{
-	    			return ok("wrong");
-	    		}
-	        }, httpExecutionContext.current());
-	    }
+	@Inject
+	public DeviceMobileController(UserRepository userRepository, HttpExecutionContext httpExecutionContext) {
+
+		this.userRepository = userRepository;
+		this.httpExecutionContext = httpExecutionContext;
+		Cache<String, String> cache = CacheBuilder.newBuilder().expireAfterWrite(20, TimeUnit.MINUTES).build();
+
+	}
+
+	public CompletionStage<Result> registeruser() {
+		JsonNode json = request().body().asJson();
+		String name = json.findPath("name").textValue();
+		String passwordEn = json.findPath("password").textValue();
+		String phone = json.findPath("phoneNumber").textValue();
+		String email = json.findPath("email").textValue();
+		String area = json.findPath("area").textValue();
+		Boolean status= userRepository.checkuserUnique(phone,name);
+		if(status)
+		{
+			return userRepository.insert(name, passwordEn,phone,email,area).thenApplyAsync(data -> {
+				return ok("ok");
+			}, httpExecutionContext.current());
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public CompletionStage<Result> mobilelogin() {
+		JsonNode json = request().body().asJson();
+		String name = json.findPath("name").textValue();
+		String passwordEn = json.findPath("password").textValue();
+		return userRepository.validUserPassword(name, passwordEn).thenApplyAsync(data -> {
+			if (data.get()) {
+				return ok("ok");
+			} else {
+				return ok("wrong");
+			}
+		}, httpExecutionContext.current());
+	}
+
+	public CompletionStage<Result> resetpassword() {
+		JsonNode json = request().body().asJson();
+		String phoneNumber = json.findPath("phoneNumber").textValue();
+		String code = json.findPath("code").textValue();
+		String passwordEn = json.findPath("password").textValue();
+		String cacheCode = cahceBuilder.getIfPresent(phoneNumber);
+		if (cacheCode == null) {
+			return null;
+			
+		} else {
+			if(code.equals(cacheCode))
+			{
+				userRepository.update(phoneNumber, passwordEn).thenApplyAsync(data -> {
+					if (data.get()) {
+						return ok("ok");
+					} else {
+						return ok("wrong");
+					}
+				}, httpExecutionContext.current());
+			}
+			else {
+				return null;
+			}
+		}
+		return null;
+	}
+
+	public Result mobilecode() {
+
+		JsonNode json = request().body().asJson();
+		String phoneNumber = json.findPath("phoneNumber").textValue();
+		try {
+			random = new Random();
+			int randNum = random.nextInt(100000);
+			String code = String.valueOf(randNum);
+			cahceBuilder.put(phoneNumber, code);
+			PhoneMessage.sendSms(phoneNumber, code);
+			System.out.println(code);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ok("ok");
+	}
 }
--- app/repository/UserRepository.java
+++ app/repository/UserRepository.java
@@ -18,55 +18,95 @@ import play.db.ebean.EbeanConfig;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 public class UserRepository {
-	    private final EbeanServer ebeanServer;
-	    private final DatabaseExecutionContext executionContext;
+	private final EbeanServer ebeanServer;
+	private final DatabaseExecutionContext executionContext;
 
-	    @Inject
-	    public UserRepository(EbeanConfig ebeanConfig, DatabaseExecutionContext executionContext) {
-	        this.ebeanServer = Ebean.getServer(ebeanConfig.defaultServer());
-	        this.executionContext = executionContext;
-	    }
+	@Inject
+	public UserRepository(EbeanConfig ebeanConfig, DatabaseExecutionContext executionContext) {
+		this.ebeanServer = Ebean.getServer(ebeanConfig.defaultServer());
+		this.executionContext = executionContext;
+	}
 
-	    public CompletionStage<Map<String, String>> options() {
-	        return supplyAsync(() -> ebeanServer.find(User.class).orderBy("username").findList(), executionContext)
-	                .thenApply(list -> {
-	                    HashMap<String, String> options = new LinkedHashMap<String, String>();
-	                    for (User c : list) {
-	                        options.put(c.id.toString(), c.username);
-	                    }
-	                    return options;
-	                });
-	    }
-	    
-	    public CompletionStage<Long> insert(String username,String password) {
-	    	User user = new User();
-	    	user.username = username;
-	    	user.password =password;
-	    	user.id = System.currentTimeMillis();
-	        return supplyAsync(() -> {
-	             ebeanServer.insert(user);
-	             return user.id;
-	        }, executionContext);
-	    }
-	    
-	    public CompletionStage<Optional<Boolean>> validUserPassword(String username,String password) {
-	    	 return supplyAsync(() -> {
-	             Transaction txn = ebeanServer.beginTransaction();
-	             Optional<Boolean> value = Optional.empty();
-	             try {
-	            	 //String decode = Base64_.base64decode(password);
-	                 User user = ebeanServer.find(User.class).where().eq("username", username).eq("password", password).findUnique();
-	                 if (user != null) {
-	                     value = Optional.of(true);
-	                 }
-	                 else
-	                 {
-	                	 value = Optional.of(false);
-	                 }
-	             } finally {
-	                 txn.end();
-	             }
-	             return value;
-	         }, executionContext);
-	    }
+	public CompletionStage<Map<String, String>> options() {
+		return supplyAsync(() -> ebeanServer.find(User.class).orderBy("username").findList(), executionContext)
+				.thenApply(list -> {
+					HashMap<String, String> options = new LinkedHashMap<String, String>();
+					for (User c : list) {
+						options.put(c.id.toString(), c.username);
+					}
+					return options;
+				});
+	}
+
+	public CompletionStage<Boolean> insert(String username, String password, String phone, String email, String area) {
+		User user = new User();
+		user.username = username;
+		user.password = password;
+		user.email = email;
+		user.phone = phone;
+		user.register_area = area;
+		user.id = System.currentTimeMillis();
+		return supplyAsync(() -> {
+			ebeanServer.insert(user);
+			return true;
+		}, executionContext);
+	}
+
+	public CompletionStage<Optional<Boolean>> update(String phoneNumber, String password) {
+		return supplyAsync(() -> {
+			Transaction txn = ebeanServer.beginTransaction();
+			Optional<Boolean> value = Optional.empty();
+			try {
+				User user = ebeanServer.find(User.class).where().eq("phone", phoneNumber).findUnique();
+				if (user != null) {
+					user.password = password;
+					user.update();
+					txn.commit();
+					value = Optional.of(true);
+				} else {
+					value = Optional.of(false);
+				}
+			} finally {
+				txn.end();
+			}
+			return value;
+		}, executionContext);
+	}
+
+	public Boolean checkuserUnique(String phoneNumber, String username) {
+		Boolean value = false;
+		try {
+			User user = ebeanServer.find(User.class).where().eq("phone", phoneNumber).eq("username", username)
+					.findUnique();
+			if (user != null) {
+				value = false;
+			} else {
+				value = true;
+			}
+		} catch (Exception e) {
+			value = false;
+		} finally {
+		}
+		return value;
+	}
+
+	public CompletionStage<Optional<Boolean>> validUserPassword(String username, String password) {
+		return supplyAsync(() -> {
+			Transaction txn = ebeanServer.beginTransaction();
+			Optional<Boolean> value = Optional.empty();
+			try {
+				// String decode = Base64_.base64decode(password);
+				User user = ebeanServer.find(User.class).where().eq("username", username).eq("password", password)
+						.findUnique();
+				if (user != null) {
+					value = Optional.of(true);
+				} else {
+					value = Optional.of(false);
+				}
+			} finally {
+				txn.end();
+			}
+			return value;
+		}, executionContext);
+	}
 }
